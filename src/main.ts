d62c8e1b14aefd29ab43e74934ed4fcc4225e44d
Antoine Gaillot <manzagregine@gmail.com>
2020-05-27 23:09:18 +0200

    feat: getting recipes

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,75 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import * as cheerio from 'cheerio';
+import * as https from 'https';
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+// Paginate recipes
+// Get on recipe
+https
+  .get('https://menu-vegetarien.com/recettes/', (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+    } else if (!/^text\/html/.test(contentType)) {
+      error = new Error(
+        'Invalid content-type.\n' +
+          `Expected text/html but received ${contentType}`,
+      );
+    }
+    if (error) {
+      console.error(error.message);
+      // Consume response data to free up memory
+      res.resume();
+      return;
+    }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
+    res.on('end', () => {
+      if (!rawData) {
+        return;
+      }
+
+      const $ = cheerio.load(rawData, {
+        normalizeWhitespace: true,
+      });
+
+      $('div.elementor-posts article').each((_, element) => {
+        const $recipe = $(element);
+        const type = $recipe.find('div.elementor-post__badge').text();
+        const photoUrl = $recipe
+          .find('div.elementor-post__thumbnail img')
+          .attr('src');
+        const title = $recipe
+          .find('div.elementor-post__text > h3.elementor-post__title')
+          .text()
+          .replace(/\n/g, '')
+          .replace(/\t/g, '');
+        const description = $recipe
+          .find(
+            'div.elementor-post__text > div.elementor-post__excerpt p:first-of-type',
+          )
+          .text();
+        const link = $recipe
+          .find('div.elementor-post__text > a.elementor-post__read-more')
+          .attr('href');
+
+        const recipe = {
+          type,
+          photoUrl,
+          title,
+          description,
+          link,
+        };
+
+        console.log(JSON.stringify(recipe, null, 2));
+      });
+    });
+  })
+  .on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
