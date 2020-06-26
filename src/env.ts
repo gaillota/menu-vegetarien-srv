9c307dfc868d50f8b647ea1962ef89f4166867d2
Dany Badr <dany@upflow.io>
2020-06-27 00:28:25 +0200

    rename env vars again

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
-export const ALGOLIA_APP_ID = process.env.ALGOLIASEARCH_APP_ID;
+export const ALGOLIA_APP_ID = process.env.ALGOLIASEARCH_APPLICATION_ID;
 export const ALGOLIA_API_KEY = process.env.ALGOLIASEARCH_API_KEY;
 export const ALGOLIA_RECIPE_INDEX_NAME = process.env.ALGOLIASEARCH_RECIPE_INDEX_NAME;
