03498dd2866fd9f3acd020b56ba3a36a90a4c3e6
Antoine Gaillot <manzagregine@gmail.com>
2020-07-04 23:02:03 +0200

    refactor: moving recipes logic to own folder

--- src/scripts/indexAllRecipes.ts
+++ src/scripts/indexAllRecipes.ts
@@ -1,16 +1,15 @@
-import getRecipes from '../resolvers/getRecipes';
-import { filterRecipe } from '../workers/recipeFilterer';
+import { getRecipesSlug } from '../recipes/getRecipesSlugs'
+import { filterRecipe } from '../workers/recipeFilterer'
 
 export async function indexAllRecipes(): Promise<void> {
-  let hasMore;
-  let currentPage = 1;
+  let hasMore
+  let currentPage = 1
   do {
-    const result = await getRecipes({ page: currentPage });
+    const result = await getRecipesSlug({ page: currentPage })
     for (const slug of result.data) {
       await filterRecipe(slug)
     }
-    hasMore = result.hasMore;
-    currentPage = result.page + 1;
-  } while (hasMore);
-
+    hasMore = result.hasMore
+    currentPage = result.page + 1
+  } while (hasMore)
 }
