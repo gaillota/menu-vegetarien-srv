c37c57f3583f716d0fda783e3d39e20ac91e10ae
Nicolas Keller <nicolas.kllr@gmail.com>
2020-07-04 22:48:15 +0200

    update worker

--- src/scripts/indexAllRecipes.ts
+++ src/scripts/indexAllRecipes.ts
@@ -1,17 +1,16 @@
 import getRecipes from '../resolvers/getRecipes';
-import { sendToQueue } from "../rabbitmq";
-import { Queue } from "../types";
+import { filterRecipe } from '../workers/recipeFilterer';
 
 export async function indexAllRecipes(): Promise<void> {
   let hasMore;
   let currentPage = 1;
-  const slugs = [];
   do {
     const result = await getRecipes({ page: currentPage });
-    slugs.push(...result.data);
+    for (const slug of result.data) {
+      await filterRecipe(slug)
+    }
     hasMore = result.hasMore;
     currentPage = result.page + 1;
   } while (hasMore);
 
-  await sendToQueue(Queue.RecipeFilterer, slugs)
 }
