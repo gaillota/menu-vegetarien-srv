5cd452908076bab74d833ddecdd6cf8811798c43
Antoine Gaillot <manzagregine@gmail.com>
2020-07-04 19:32:01 +0200

    core: koa server

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import * as Koa from 'koa';
+import * as Router from '@koa/router';
+import * as logger from 'koa-logger';
+import * as cors from '@koa/cors';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    allowHeaders:
+      'Authorization, Origin, X-Requested-With, Content-Type, Accept',
+    keepHeadersOnError: true,
+  }),
+);
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.body = {
+      type: err.message || '',
+      original: err,
+    };
+    ctx.status = err.status || 500;
+  }
+});
+app.use(logger());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🚀  Server ready at http://localhost:${process.env.PORT || 3000}`);
+});
