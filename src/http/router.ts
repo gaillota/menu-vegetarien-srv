334207ed5d42553c63f1f62b163ceed081fd24c9
Antoine Gaillot <manzagregine@gmail.com>
2020-07-04 22:13:56 +0200

    feat: http server

---
+++ src/http/router.ts
@@ -0,0 +1,13 @@
+import * as Router from '@koa/router'
+import { Context } from "koa";
+import { indexAllRecipes } from '../scripts/indexAllRecipes'
+
+const router = new Router()
+
+router.get('/index-new-recipes', async (ctx: Context) => {
+  await indexAllRecipes()
+  ctx.status = 200
+  ctx.body = 'Ok'
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,19 @@
-require('dotenv').config();
-import * as Koa from 'koa';
-import * as Router from '@koa/router';
-import * as logger from 'koa-logger';
-import * as cors from '@koa/cors';
-import * as signale from 'signale';
-import { initRabbit } from './rabbitmq';
-import { initRedis } from "./redis";
+require('dotenv').config()
+import * as https from 'http'
+import * as signale from 'signale'
+import { initHttp } from './http'
+import { initRabbit } from './rabbitmq'
+import { initRedis } from './redis'
 
-const app = new Koa()
-const router = new Router()
-
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-    allowHeaders:
-      'Authorization, Origin, X-Requested-With, Content-Type, Accept',
-    keepHeadersOnError: true,
-  }),
-)
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (err) {
-    ctx.body = {
-      type: err.message || '',
-      original: err,
-    }
-    ctx.status = err.status || 500
-  }
-})
-app.use(logger())
-app.use(router.routes())
-app.use(router.allowedMethods())
-
-Promise.all([initRabbit(), initRedis()])
-  .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      signale.success(`🚀  Server ready at http://localhost:${process.env.PORT || 3000}`);
-    });
+Promise.all([initHttp(), initRabbit(), initRedis()])
+  .then(([app]) => {
+    https.createServer(app.callback()).listen(process.env.PORT || 3000, () => {
+      signale.success(
+        `🚀  Server ready at http://localhost:${process.env.PORT || 3000}`,
+      )
+    })
   })
   .catch((error) => {
     signale.error(error)
     signale.error(`Could not init server`)
   })
-
