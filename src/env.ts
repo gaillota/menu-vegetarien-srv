efaa0da72fa2bf42d7a388a70ac072f4a7c57282
Nicolas Keller <nicolas.kllr@gmail.com>
2020-07-04 20:44:21 +0200

    setup rabbit workers

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,4 @@ export const ALGOLIA_API_KEY = process.env.ALGOLIASEARCH_API_KEY
 export const ALGOLIA_RECIPE_INDEX_NAME =
   process.env.ALGOLIASEARCH_RECIPE_INDEX_NAME
 export const REDIS_URL = process.env.REDIS_URL
+export const CLOUDAMQP_URL = process.env.CLOUDAMQP_URL;
--- src/rabbitmq/index.ts
+++ src/rabbitmq/index.ts
@@ -1,18 +1,53 @@
+import * as amqp from 'amqplib';
+import { CLOUDAMQP_URL } from '../env';
+import * as globCb from 'glob';
+import * as path from 'path';
+import * as util from 'util';
 import { Queue } from '../types';
-import amqp from 'amqp';
 
+const glob = util.promisify(globCb)
 
-export async function initTopology(): Promise<void> {
-  const queues = Object.values(Queue)
+let channel = null
 
-  for (queue of queues) {
+async function initTopology(): Promise<void> {
+  if (!channel) {
+    throw new Error('Call initRabbit first')
+  }
 
+  for (const queue of Object.values(Queue)) {
+    await channel.assertQueue(queue, { durable: true })
   }
 }
 
 export async function initRabbit(): Promise<void> {
+  const conn = await amqp.connect(CLOUDAMQP_URL)
+  channel = await conn.createChannel()
+
+  await initTopology()
+
+  const workerPaths = await glob('/*.js', { root: path.join(__dirname, '../workers') })
+  const workers = workerPaths.map(require)
+
+  for (const worker of workers) {
+    await channel.consume(worker.queue, async(message) => {
+      try {
+        const content = message.content.toString()
+        const parsed = JSON.parse(content)
+
+        await worker.work(parsed)
+
+        await channel.ack(message)
+      } catch (error) {
+        return channel.nack(message, false, false)
+      }
+    })
+  }
 }
 
 export async function sendToQueue(queue: Queue, message: object): Promise<void> {
+  if (!channel) {
+    throw new Error('Call initRabbit first')
+  }
 
+  await channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true, })
 }
