ef8985e65ef635945a8d5b46f6cde4e203f2063d
Antoine Gaillot <manzagregine@gmail.com>
2020-07-22 13:10:26 +0200

    refactor: moving computing away from recipe parser

--- src/recipes/getRecipe.ts
+++ src/recipes/getRecipe.ts
@@ -1,11 +1,15 @@
 import { Recipe } from '../types'
 import parseRecipe from '../parsers/recipe'
 import { api } from '../api'
+import { dateToTimestamp } from "../utils";
 
 export async function getRecipe(slug: string): Promise<Recipe> {
   const html = await api(`/recettes/${slug}`)
   const recipe = parseRecipe(html)
+  const createdAtTimestamp = dateToTimestamp(recipe.createdAt)
+
   recipe.slug = slug
+  recipe.createdAtTimestamp = createdAtTimestamp
 
   return recipe
 }
