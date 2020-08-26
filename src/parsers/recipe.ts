991c2cb8ef5a6958d0b27eacc8cd012186e39da5
Antoine Gaillot <manzagregine@gmail.com>
2020-08-27 00:24:00 +0200

    refactor: useless space

--- src/parsers/recipe.ts
+++ src/parsers/recipe.ts
@@ -46,8 +46,6 @@ function parseRecipe(html: string): Recipe {
         .each((_, element) => {
           const ingredient = $(element).text()
 
-
-
           currentIngredients.push(ingredient)
         })
 
