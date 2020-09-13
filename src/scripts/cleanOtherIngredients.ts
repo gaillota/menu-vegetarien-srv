c1fdd4ec41c7c3873c1b97d5f3eb13fabc7e59f7
Antoine Gaillot <manzagregine@gmail.com>
2020-09-14 00:34:35 +0200

    scripts: fix other ingredients bug

---
+++ src/scripts/cleanOtherIngredients.ts
@@ -0,0 +1,36 @@
+import algoliasearch from 'algoliasearch'
+import { ALGOLIA_API_KEY, ALGOLIA_APP_ID } from '../env'
+
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
+const recipesIndex = client.initIndex('recipes')
+
+export const description =
+  'update recipe other ingredients and remove empty array'
+
+export async function main(): Promise<void> {
+  let hits = []
+
+  return recipesIndex
+    .browseObjects({
+      query: '',
+      batch: (batch) => {
+        hits = hits.concat(batch)
+      },
+    })
+    .then(() => {
+      console.log('updating objects')
+      hits = hits.map((hit) => ({
+        ...hit,
+        otherIngredients:
+          hit.otherIngredients &&
+          hit.otherIngredients.length === 1 &&
+          !hit.otherIngredients[0].quantity
+            ? undefined
+            : hit.otherIngredients,
+      }))
+    })
+    .then(async () => {
+      console.log('saving objects')
+      await recipesIndex.saveObjects(hits).wait()
+    })
+}
