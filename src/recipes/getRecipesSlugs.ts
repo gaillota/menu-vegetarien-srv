8ffd715d4c192cd170c9f40f5c8e5c137d779a38
Antoine Gaillot <manzagregine@gmail.com>
2020-10-13 23:26:49 +0200

    refactor: logging recipes page

--- src/recipes/getRecipesSlugs.ts
+++ src/recipes/getRecipesSlugs.ts
@@ -1,8 +1,12 @@
 import * as request from 'request-promise'
+import { Signale } from 'signale'
 
 import parseRecipesSlugs from '../parsers/recipes'
 import { baseUrl } from '../constants'
 import { PaginationResult } from '../types'
+import * as chalk from "chalk";
+
+const signale = new Signale({ scope: 'http' })
 
 function buildUrl({ page }): string {
   return `${baseUrl}/recettes${page > 1 ? `/page/${page}` : ''}`
@@ -11,6 +15,9 @@ function buildUrl({ page }): string {
 export async function getRecipesSlugs({ page = 1 } = {}): Promise<
   PaginationResult<string>
 > {
+  signale.await(
+    chalk`Parsing recipes page {yellow #${page}}...`,
+  )
   const url = buildUrl({ page })
   const result = await request.get(url)
   const { data, pagesCount } = parseRecipesSlugs(result)
