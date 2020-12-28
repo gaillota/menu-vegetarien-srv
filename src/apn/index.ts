e62b897ef2444bbf5bd88a5820285e3f7d342211
Antoine Gaillot <manzagregine@gmail.com>
2020-12-28 19:50:35 +0200

    refactor: apn key rename

--- src/apn/index.ts
+++ src/apn/index.ts
@@ -8,7 +8,7 @@ const isProduction = env.ENV === 'production'
 // configuring APN with credentials
 const apnOptions = {
   token: {
-    key: path.join(__dirname, '../../../certs/AuthKey_2RJC6G83YU.p8'),
+    key: path.join(__dirname, '../../../certs/APN_AuthKey.p8'),
     keyId: env.APN_KEY_ID,
     teamId: env.APN_TEAM_ID,
   },
